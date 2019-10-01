458b9dbefcde87ba3d00f4aaad8446b235bee09e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-10-01 22:26:16 +0000

    Deploy aleks-nek/react-travis-ci-demo to github.com/aleks-nek/react-travis-ci-demo.git:gh-pages

---
+++ precache-manifest.50c0656de9e5533e33a6d0134c2b944f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7bc16c316fe76f7512fcc7dfd0cdb63",
+    "url": "/react-travis-ci-demo/index.html"
+  },
+  {
+    "revision": "1c10ba4e0081391c2866",
+    "url": "/react-travis-ci-demo/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "e0c7f3a41cabad422bf6",
+    "url": "/react-travis-ci-demo/static/js/2.4ff1efb0.chunk.js"
+  },
+  {
+    "revision": "1c10ba4e0081391c2866",
+    "url": "/react-travis-ci-demo/static/js/main.0bb75dfa.chunk.js"
+  },
+  {
+    "revision": "504e3d374b722c89edfa",
+    "url": "/react-travis-ci-demo/static/js/runtime-main.a963fb28.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react-travis-ci-demo/static/media/logo.25bf045c.svg"
+  }
+]);
